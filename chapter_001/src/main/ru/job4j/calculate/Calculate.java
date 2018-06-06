f9c8b20dd236e2b9f7143166dc8b0a31183fee06
eonegin <e.onegin87@mail.ru>
2018-06-06 22:23:18 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,19 @@
 package ru.job4j.calculate;
 
+/**
+*Calculate. 
+*@author eonegin (3he@mail.ru
+*@since 06.06.2018
+*@version 1
+*/
+
 public class Calculate{
+
+/**
+*Main. Вывод строки на экран.
+*@param args - args.
+*/
+
 public static void main(String[] args) {
 System.out.print("Hello world");
 }
