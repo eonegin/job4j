2108225ee09c7ea6621a2f39e74101ee2e5452a4
eonegin <e.onegin87@mail.ru>
2018-06-08 22:12:00 +0300

    1.4. JUnit

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,29 @@
+package ru.job4j.calculate;
+
+/**
+*Calculate. 
+*@author eonegin (3he@mail.ru
+*@since 06.06.2018
+*@version 1
+*/
+
+public class Calculate{
+
+/**
+*Main. Вывод строки на экран.
+*@param args - args.
+*/
+
+public static void main(String[] args) {
+System.out.print("Evgeny Onegin");
+}
+
+/**
+* Method echo.
+* @param name Your name.
+* @return Echo plus your name.
+*/
+public String echo(String name) {
+    return "Echo, echo, echo : " + name;
+}
+}
