e18cbbc2d706e953a505c44b460607fc6b48edfa
eonegin <e.onegin87@mail.ru>
2018-06-28 23:22:47 +0300

    6.4. Слова начинается с ...

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -21,9 +21,13 @@ public class ArrayChar {
         boolean result = true;
         char[] value = prefix.toCharArray();
         for (int i = 0; i < value.length; i++) {
-            if (data[i] == value[i]);
-            else result = false;
+            if (data[i] != value[i])
+            {
+                result = false;
+                break;
+            }
         }
         return result;
     }
 }
+
