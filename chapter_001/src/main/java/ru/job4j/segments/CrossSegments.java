b2f430bd9a71e07629e4e7c2376c488cc4e502b1
eonegin <e.onegin87@mail.ru>
2018-07-25 19:55:16 +0300

    Определение пересечения отрезков (исправлен одиночный |)

--- chapter_001/src/main/java/ru/job4j/segments/CrossSegments.java
+++ chapter_001/src/main/java/ru/job4j/segments/CrossSegments.java
@@ -5,7 +5,7 @@ public class CrossSegments {
     static boolean result = false;
 
     public static boolean segments(int a, int b, int c, int d) {
-        if ((c <= b && c >= a) | (d <= b && d >= a)) {
+        if ((c <= b && c >= a) || (d <= b && d >= a)) {
             result = true;
         }
         return result;
