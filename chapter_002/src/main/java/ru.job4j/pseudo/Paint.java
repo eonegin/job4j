2db2032daaafdb1504e55a28a8884d474bcb51f1
eonegin <e.onegin87@mail.ru>
2018-10-22 23:30:55 +0300

    4. Используя шаблон проектирования - стратегию

---
+++ chapter_002/src/main/java/ru.job4j/pseudo/Paint.java
@@ -0,0 +1,7 @@
+package ru.job4j.pseudo;
+
+public class Paint {
+    public static void draw(Shape shape) {
+        System.out.println(shape.draw());
+    }
+}
---
+++ chapter_002/src/main/java/ru.job4j/pseudo/Shape.java
@@ -0,0 +1,5 @@
+package ru.job4j.pseudo;
+
+interface Shape {
+    public String draw();
+}
