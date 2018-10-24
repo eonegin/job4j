3af1d42fcc816804501de261c0e2392f2801422e
eonegin <e.onegin87@mail.ru>
2018-10-24 21:48:10 +0300

    5. Рефакторинг теста @Before @After

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -7,6 +7,15 @@ import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 
 /**
  * @author Evgeniy Onegin
@@ -16,6 +25,21 @@ import static org.junit.Assert.assertThat;
 
 
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
         // получаем ссылку на стандартный вывод в консоль.
